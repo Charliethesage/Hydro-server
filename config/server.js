e247d045fbf2bd4cea92246bca0e99f4d5c056df
Charliethesage <rubancharles99@gmail.com>
2022-12-12 18:40:40 +0530

    fdfdf

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('QoQ7nfbmFrcAFlqkLzWBbg==,SELhoYfWShYVpm/XBb07yw==,HNBFipozbmVjU8WpjoA8OQ==,KIqSiF8hNl/L7wwidwSxEQ=='),
   },
 });
