a72ed70f47109337e3bc1b5c330000f868a0392d
Charliethesage <rubancharles99@gmail.com>
2022-12-12 20:24:11 +0530

    gddgdgd

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','P7XjuVrsLt+9ZZ3u3xJuCQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','NGA2bdZdFJEZtD6jGP/rzw=='),
   },
 });
