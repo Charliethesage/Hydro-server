3d359f717ef6832e1a6c8f99014c893af2138d5e
Charliethesage <rubancharles99@gmail.com>
2022-12-20 22:32:38 +0530

    svsv

---
+++ config/plugins.js
@@ -0,0 +1,37 @@
+//const nodemailerNTLMAuth = require('nodemailer-ntlm-auth');
+
+module.exports = ({ env }) => ({
+//  email plugin
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port:  587,
+          auth: {
+            user:"navalhydrography@gmail.com" ,
+            pass:"gfsohashxwjzqgpk", 
+          },
+        },
+        settings: {
+          defaultFrom: 'navalhydrography@gmail.com',
+          defaultReplyTo: 'navalhydrography@gmail.com',
+        },
+      },
+    },
+/* uploads plugin
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
+      }, */
+  });   
