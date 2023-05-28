11dc5eb2ba318c4d33216c213b47764b41d05ce1
Charlie963 <rubancharles99@gmail.com>
2023-05-28 23:41:56 +0530

    lint fix

--- config/database.js
+++ config/database.js
@@ -3,13 +3,14 @@
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA'), // Not required
-          ssl:  env.bool('DATABASE_SSL', true),
+          host: env('DATABASE_HOST', 'dpg-chpfc5ndvk4goeo1bp10-a.singapore-postgres.render.com'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'naval_database'),
+          user: env('DATABASE_USERNAME', 'admin'),
+          password: env('DATABASE_PASSWORD', '3YkeE0zvVBt8CWMbppYBE666flxC3Pvo'),
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
         },
         debug: false,
       },
